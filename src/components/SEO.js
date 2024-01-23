206063054fd5800311c23df6a25e8e75444b31bd
lstrucelj <strucelj.leona@gmail.com>
2024-01-23 18:09:45 +0100

    Tabs changes

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -33,7 +33,7 @@ export default ({ serviceNode, servicePath, serviceSEO }) => {
       : facebookImage;
   } else {
     title = config.siteTitle;
-    titleHead = `${config.siteTitle} - ${config.siteDescription}`;
+    titleHead = config.siteTitle;
     description = config.siteDescription;
     image = config.siteUrl + "/" + config.siteLogo;
   }
@@ -132,19 +132,19 @@ export default ({ serviceNode, servicePath, serviceSEO }) => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.webp"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.webp"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.webp"
+        href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
